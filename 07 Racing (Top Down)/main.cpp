20c3aca9ddd01ac1ae42b4b0fa8c2583c35cb97a
AHMED RAYEN <131598929+rayen-feb@users.noreply.github.com>
2024-01-03 10:26:36 -1100

    Update main.cpp
    add struct point

--- 07 Racing (Top Down)/main.cpp
+++ 07 Racing (Top Down)/main.cpp
@@ -1,2 +1,12 @@
 #include <SFML/Graphics.hpp>
 using namespace sf;
+nt points[num][2] = {300, 610,
+                      1270,430,
+                      1380,2380,
+                      1900,2460,
+                      1970,1700,
+                      2550,1680,
+                      2560,3150,
+                      500, 3300};
+
+struct Car
