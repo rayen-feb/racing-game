bdc398f44d8138b94213764598cb00ec72e6e471
AHMED RAYEN <131598929+rayen-feb@users.noreply.github.com>
2024-01-03 10:30:38 -1100

    Update main.cpp
    loading images

--- 07 Racing (Top Down)/main.cpp
+++ 07 Racing (Top Down)/main.cpp
@@ -35,3 +35,8 @@ int main()
 {
     RenderWindow app(VideoMode(640, 480), "Car Racing Game!");
 	app.setFramerateLimit(60);
+       Texture t1,t2,t3;
+    t1.loadFromFile("images/background.png");
+    t2.loadFromFile("images/car.png");
+    t1.setSmooth(true);
+    t2.setSmooth(true);
